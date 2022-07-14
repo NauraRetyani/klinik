fc37159a93754c43fe30430a98f5051d968b8d36
naura <naura.retyani@gmail.com>
2022-07-14 13:42:47 +0700

    -

--- src/main/java/com/empat/klinik/controller/HomeController.java
+++ src/main/java/com/empat/klinik/controller/HomeController.java
@@ -1,6 +1,5 @@
-package com.empat.klinik.Controller;
+package com.empat.klinik.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
